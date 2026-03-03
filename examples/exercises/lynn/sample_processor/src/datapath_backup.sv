40f5c6f0bcda0d05466c0637864bdb47eb92d28f
SadhviNarayanan <sadhvin@gmail.com>
2026-03-02 19:40:26 -0800

    csr and counter logic

--- examples/exercises/lynn/sample_processor/src/datapath_backup.sv
+++ examples/exercises/lynn/sample_processor/src/datapath_backup.sv
@@ -312,18 +312,12 @@ module datapath(input  logic clk, reset,
 
   // Performance counter increment signals
   logic IncrementInstret, IncrementAdd, IncrementBranch, IncrementBranchTaken;
-  logic IncrementJumps, IncrementLoads, IncrementStores, IncrementStalls, IncrementFlushes;
 
   // signals for csr reg stuff, might need to come back to check
   assign IncrementInstret = RegWriteM || MemWriteM || LoadM;                          // From Writeback
   assign IncrementAdd = (ALUControlW == 4'b0000) && RegWriteW;  // From Writeback
   assign IncrementBranch = BranchE;                             // From Execute
   assign IncrementBranchTaken = BranchE && (PCSrcE == 2'b01);   // From Execute
-  assign IncrementJumps = JumpE;                             // From Execute
-  assign IncrementLoads = LoadM;                                   // From Memory
-  assign IncrementStores = MemWriteM;                                // From Memory
-  assign IncrementStalls = StallD;                            // From Hazard Unit
-  assign IncrementFlushes = FlushE;                          // From Hazard Unit
 
   CsrRegFile CsrRegFile(
    .clk(clk),
@@ -337,12 +331,7 @@ module datapath(input  logic clk, reset,
    .IncrementInstret(IncrementInstret),
    .IncrementAdd(IncrementAdd),
    .IncrementBranch(IncrementBranch),
-   .IncrementBranchTaken(IncrementBranchTaken),
-   .IncrementJumps(IncrementJumps),
-   .IncrementLoads(IncrementLoads),
-   .IncrementStores(IncrementStores),
-   .IncrementStalls(IncrementStalls),
-   .IncrementFlushes(IncrementFlushes)
+   .IncrementBranchTaken(IncrementBranchTaken)
  );
 
   // because we are reading on the same cycle, timing should be fine for oldCSRReadDataM
